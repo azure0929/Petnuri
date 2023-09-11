71e43f7c245c0cda20602d2ce9ff95ee405c06d8
hwanginseung <hwanginseung@naver.com>
2023-09-11 21:52:33 +0900

    #12 Design: 바텀시트 anime 부여

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import styles from '@/styles/bottomsheet.module.scss'
 import { useRecoilState } from 'recoil'
 import { bottomSheetState } from "@/store";
@@ -9,19 +9,31 @@ interface BottomSheetProps {
 
 const BottomSheet: React.FC<BottomSheetProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useRecoilState(bottomSheetState);
+  const [isVisible, setIsVisible] = useState(false);
 
-   return (
-     <>
-        {isOpen && (
-          <>
-            <div className={styles.background} onClick={() => setIsOpen(false)}></div>
-            <div className={`${styles.container} ${isOpen ? styles.active : ''}`}>
-              {children}
-            </div>
-          </>
-        )}
-     </>
-   );
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+      {isOpen && (
+        <>
+          <div
+            className={`${styles.background} ${isVisible ? styles.visible : ''}`}
+            onClick={() => setIsOpen(false)}
+          ></div>
+          <div className={`${styles.container} ${isVisible ? styles.visible : ''}`}>
+            {children}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
-export default BottomSheet
+export default BottomSheet;
