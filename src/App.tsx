c2ee7869b38ae5da3100d053b1f65fc341dd47e5
LeejungHwan <antglljung@naver.com>
2023-09-12 17:31:20 +0900

    feat: 야너도? 야나두! 페이지 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Place from "./pages/Place";
 import Challenge from "./pages/challenge";
 import PointShop from "@/pages/challenge/PointShop";
+import EvnetChallenge1 from "./pages/challenge/EventChallenge1";
 import PetTalk from "./pages/PetTalk";
 import MyPage from "./pages/MyPage";
 
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/place" element={<Place />} />
       <Route path="/challenge" element={<Challenge />} />
       <Route path="/pointshop" element={<PointShop />} />
+      <Route path="/eventchallenge1" element={<EvnetChallenge1 />} />
       <Route path="/pettalk" element={<PetTalk />} />
       <Route path="/mypage" element={<MyPage />} />
     </Routes>
--- src/pages/challenge/index.tsx
+++ src/pages/challenge/index.tsx
@@ -1,13 +1,13 @@
 import MainTab from "@/components/MainTab";
 import Background from "@/components/Background";
-import styles from '@/styles/challenge.module.scss'
+import styles from "@/styles/challenge.module.scss";
 import { useRef, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import fireImage from '/fire.png'
-import creditImage from '/credit.png'
-import { FiRotateCcw } from 'react-icons/fi'
+import { Link, useNavigate } from "react-router-dom";
+import fireImage from "/fire.png";
+import creditImage from "/credit.png";
+import { FiRotateCcw } from "react-icons/fi";
 import ChallengeHBS from "@/components/BottomSheet/ChallengeHBS";
-import { useSetRecoilState } from 'recoil'
+import { useSetRecoilState } from "recoil";
 import { bottomSheetState } from "@/store";
 import { createToast } from "@/utils/ToastUtils";
 
@@ -15,16 +15,16 @@ const Challenge = () => {
   const scrollRef = useRef(null);
   const intervalId = useRef(0);
   const setBottomIsOpen = useSetRecoilState(bottomSheetState);
-  const [hour,setHour] = useState(0);
+  const [hour, setHour] = useState(0);
   const [participated, setParticipated] = useState(false);
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     const calculateTime = () => {
       const now = new Date();
       const nextDay = new Date(now);
       nextDay.setDate(now.getDate() + 1);
-      nextDay.setHours(0, 0, 0, 0); 
+      nextDay.setHours(0, 0, 0, 0);
 
       let diffInMs = nextDay.getTime() - now.getTime();
       let diffInHours = Math.floor(diffInMs / (1000 * 60 * 60));
@@ -37,25 +37,25 @@ const Challenge = () => {
     };
     setTimeout(() => {
       calculateTime();
-      
+
       intervalId.current = setInterval(calculateTime, 60000);
     }, 1000);
 
     return () => {
-      if(intervalId.current) clearInterval(intervalId.current); 
+      if (intervalId.current) clearInterval(intervalId.current);
     };
-}, []);
+  }, []);
 
   const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
     if (!scrollRef.current) return;
     if (e.deltaY === 0) return;
-  
+
     e.preventDefault();
     const current = scrollRef.current as HTMLDivElement;
     current.scrollLeft += e.deltaY;
   };
 
-  const wrong = () => createToast('error', '추후 오픈 예정입니다');
+  const wrong = () => createToast("error", "추후 오픈 예정입니다");
 
   return (
     <>
@@ -69,15 +69,19 @@ const Challenge = () => {
           <div className={styles.date}> 23.03.06</div>
         </div>
         <div className={styles.credit}>
-         1220 크레딧 
-         <img src={creditImage} alt="credit" />
+          1220 크레딧
+          <img src={creditImage} alt="credit" />
         </div>
 
         <div className={styles.shop}>
           <div className={styles.container}>
-            <Link to="/pointshop" className={styles.button}>포인트샵 가기</Link>
+            <Link to="/pointshop" className={styles.button}>
+              포인트샵 가기
+            </Link>
             <span>|</span>
-            <button className={styles.button_right} onClick={wrong}>포인트 사용 내역</button>
+            <button className={styles.button_right} onClick={wrong}>
+              포인트 사용 내역
+            </button>
           </div>
         </div>
 
@@ -85,14 +89,18 @@ const Challenge = () => {
           <div className={styles.head}>
             <span>이벤트 챌린지</span>
           </div>
-          <div 
-          className={styles.body} 
-          ref={scrollRef}
-          onWheel={handleWheel}>
+          <div className={styles.body} ref={scrollRef} onWheel={handleWheel}>
             <div className={styles.section}>
-              <div className={styles.img}></div>
+              <div
+                className={styles.img}
+                onClick={() => navigate("/eventchallenge1")}
+              >
+                asd
+              </div>
               <div className={styles.text}>
-                <div className={styles.title}>랜선대회 챌린지</div>
+                <Link to="/eventchallenge1" className={styles.title}>
+                  랜선대회 챌린지
+                </Link>
                 <div className={styles.subtitle}>카드 디자인 서브 타이틀</div>
               </div>
             </div>
@@ -110,35 +118,40 @@ const Challenge = () => {
           <div className={styles.challenge}>
             <div className={styles.img}></div>
             <div className={styles.main}>
-              <div className={styles.title}>대규모 챌린지</div> 
-              <div className={styles.time}><FiRotateCcw/> {hour}시간 후 </div>
+              <div className={styles.title}>대규모 챌린지</div>
+              <div className={styles.time}>
+                <FiRotateCcw /> {hour}시간 후{" "}
+              </div>
             </div>
             {/* f5시 참여완료를 참여하기로 바뀌지 않게 하려면 백엔드 필요  */}
             {participated ? (
-              <button 
+              <button
                 className={styles.participate_off}
-                onClick={() => setParticipated(false)} 
+                onClick={() => setParticipated(false)}
                 disabled
-                >
-                참여완료</button>
-              ) : (
-              <button 
+              >
+                참여완료
+              </button>
+            ) : (
+              <button
                 className={styles.participate_on}
-                onClick={() => setParticipated(true)} 
-                >
-                참여하기</button>
-              )}
+                onClick={() => setParticipated(true)}
+              >
+                참여하기
+              </button>
+            )}
           </div>
         </div>
         <div className={styles.bonus}>
           <div className={styles.title}>추가 보너스 받기</div>
-          <button 
-            className={styles.button} 
-            onClick={() => setBottomIsOpen(true)}>
-            보너스 받기 
+          <button
+            className={styles.button}
+            onClick={() => setBottomIsOpen(true)}
+          >
+            보너스 받기
           </button>
         </div>
-        <ChallengeHBS/>
+        <ChallengeHBS />
         <MainTab />
       </Background>
     </>
