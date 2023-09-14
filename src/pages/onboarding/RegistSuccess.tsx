6c8ab284422665fb85a33bc252fec4a15ef37b4f
azure0929 <chary8028@naver.com>
2023-09-14 18:44:07 +0900

    #54 Feat: RegistSuccess 레이아웃 작업

--- src/pages/onboarding/RegistSuccess.tsx
+++ src/pages/onboarding/RegistSuccess.tsx
@@ -1,10 +1,33 @@
 import Background from "@/components/Background";
+import arrow_left_mid from "@/assets/arrow_left_mid.svg";
+import styles from '@/styles/registsuccess.module.scss';
 
 const RegistSuccess = () => {
   return (
     <>
       <Background>
-
+        <div className={styles.head}>
+          <div role="button" className={styles.prev}>
+            <img src={arrow_left_mid} alt="prev" />
+          </div>
+          <p>펫 정보 등록</p>
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.select}>
+            <p className={styles.title}>우리아이 등록에 성공했습니다!</p>
+            <span>펫누리의 공간에 여러분을 초대합니다!</span>
+          </div>
+        </div>
+        <div className={styles.lottie}>
+          <p>로티파일</p>
+        </div>
+        <div className={styles.next}>
+          <div
+            role="button"
+          >
+            펫누리 시작하기
+          </div>
+        </div>
       </Background>
     </>
   );
