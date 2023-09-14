49bd263919adb4215befec323f0c81bfbf473a3b
azure0929 <chary8028@naver.com>
2023-09-14 18:47:34 +0900

    #54 Feat: 이전 및 펫누리 시작하기 버튼 Navigate 기능 추가

--- src/pages/onboarding/RegistSuccess.tsx
+++ src/pages/onboarding/RegistSuccess.tsx
@@ -1,14 +1,25 @@
 import Background from "@/components/Background";
+import { useNavigate } from "react-router-dom";
 import arrow_left_mid from "@/assets/arrow_left_mid.svg";
 import styles from '@/styles/registsuccess.module.scss';
 
 const RegistSuccess = () => {
 
+  const navigate = useNavigate();
+
+  const onPetRegist = () => {
+    navigate(`/petregist`);
+  }
+
+  const onMain = () => {
+    navigate(`/`);
+  }
+
   return (
     <>
       <Background>
         <div className={styles.head}>
-          <div role="button" className={styles.prev}>
+          <div role="button" className={styles.prev} onClick={onPetRegist}>
             <img src={arrow_left_mid} alt="prev" />
           </div>
           <p>펫 정보 등록</p>
@@ -25,6 +36,7 @@ const RegistSuccess = () => {
         <div className={styles.next}>
           <div
             role="button"
+            onClick={onMain}
           >
             펫누리 시작하기
           </div>
