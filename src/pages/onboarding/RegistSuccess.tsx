517b7d6063e640e6f95b5b8793068148ae06c530
azure0929 <chary8028@naver.com>
2023-09-14 18:42:20 +0900

    #54 Feat: RegistSuccess 페이지 생성

---
+++ src/pages/onboarding/RegistSuccess.tsx
@@ -0,0 +1,13 @@
+import Background from "@/components/Background";
+
+const RegistSuccess = () => {
+  return (
+    <>
+      <Background>
+
+      </Background>
+    </>
+  );
+};
+
+export default RegistSuccess;
