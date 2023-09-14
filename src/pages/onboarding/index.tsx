4292817761d26250e817d57829d536d37ad83a7e
azure0929 <chary8028@naver.com>
2023-09-14 16:13:16 +0900

    #47 Feat: onboarding 메인 페이지 생성

---
+++ src/pages/onboarding/index.tsx
@@ -0,0 +1,13 @@
+import Background from "@/components/Background";
+
+const OnBoarding = () => {
+  return (
+    <div>
+      <Background>
+        
+      </Background>
+    </div>
+  )
+}
+
+export default OnBoarding;
