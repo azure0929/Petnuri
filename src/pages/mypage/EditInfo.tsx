242d44bc9f1b3967527e84afec33c85296d759f9
saeyeon <saeyeon220@daum.net>
2023-09-15 10:40:43 +0900

    #9 style:회원탈퇴모달 완성

--- src/pages/mypage/EditInfo.tsx
+++ src/pages/mypage/EditInfo.tsx
@@ -3,8 +3,11 @@ import styles from '@/styles/editinfo.module.scss';
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import { IoIosAdd } from 'react-icons/io';
+import { useState } from 'react';
+import Warning from '../../assets/Warning.png';
 
 const EditInfo = () => {
+  const [modal, setModal] = useState(false);
   const navigate = useNavigate();
   const onClickBack = () => {
     navigate(-1);
@@ -47,13 +50,47 @@ const EditInfo = () => {
             </div>
           </div>
         </div>
-        <div className={styles.exit}>
+        <div
+          className={styles.exit}
+          onClick={() => setModal(true)}
+        >
           회원 탈퇴
           <AiOutlineRight />
         </div>
         <div className={styles.editbtnarea}>
           <button>수정 완료</button>
         </div>
+
+        {modal === true ? (
+          <div className={styles.modalcontain}>
+            <div className={styles.header}>
+              <AiOutlineLeft
+                className={styles.modalicon}
+                onClick={() => setModal(false)}
+              />
+              회원 탈퇴
+            </div>
+            <div className={styles.warningarea}>
+              <div className={styles.warning}>
+                <img
+                  src={Warning}
+                  alt="warning icon"
+                />
+                <div className={styles.warningtitle}>
+                  정말 탈퇴하시겠습니까?
+                </div>
+                <div className={styles.warningsubtitle}>
+                  회원 탈퇴 시 고객님의 모든 정보가 소멸되며 <br />
+                  이전으로 복구가 불가능합니다
+                </div>
+              </div>
+              <div></div>
+            </div>
+            <div className={styles.btnarea}>
+              <button>회원 탈퇴</button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </Background>
   );
