a823a7c1b294630ed16b31357eec01dc04746649
saeyeon <saeyeon220@daum.net>
2023-09-15 12:51:07 +0900

    #9 feat: 프로필이미지 파일 연결, 닉네임 유효성 검사

--- src/pages/mypage/EditInfo.tsx
+++ src/pages/mypage/EditInfo.tsx
@@ -8,9 +8,33 @@ import Warning from '../../assets/Warning.png';
 
 const EditInfo = () => {
   const [modal, setModal] = useState(false);
+  const [filemodal, setFilemodal] = useState(false);
+  const [input, setInput] = useState('');
+  const [validation, setValidation] = useState(false);
+  const [overlab, setOverlab] = useState(true);
   const navigate = useNavigate();
   const onClickBack = () => {
     navigate(-1);
+  };
+
+  const changeHandler = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setInput(target.value);
+    // 정규 표현식 패턴
+    var pattern = /^[a-zA-Z가-힣]+$/;
+
+    // 입력값을 패턴과 비교하여 유효성을 검사
+    if (
+      pattern.test(target.value) &&
+      target.value.length >= 2 &&
+      target.value.length <= 10
+    ) {
+      console.log('입력값이 유효합니다.');
+      setValidation(true);
+    } else {
+      console.log('입력값이 유효하지 않습니다.');
+      setValidation(false);
+    }
   };
   return (
     <Background>
@@ -26,7 +50,7 @@ const EditInfo = () => {
           <div className={styles.photoarea}>
             <div className={styles.photo}>
               <div className={styles.plusbtn}>
-                <IoIosAdd />
+                <IoIosAdd onClick={() => setFilemodal(true)} />
               </div>
             </div>
           </div>
@@ -38,10 +62,42 @@ const EditInfo = () => {
         <div className={styles.contents}>
           <div className={styles.nickinputarea}>
             닉네임
-            <div className={styles.nickinput}>
-              <input placeholder="수정을 원하실 경우 입력해주세요" />
-              <button>중복체크</button>
+            <div
+              className={styles.nickinput}
+              style={
+                validation
+                  ? { border: 'none' }
+                  : { border: '1px solid #f42a3b' }
+              }
+            >
+              <input
+                placeholder="수정을 원하실 경우 입력해주세요"
+                value={input}
+                onChange={changeHandler}
+                style={validation ? { color: '#000000' } : { color: '#f42a3b' }}
+              />
+              <button
+                style={
+                  validation
+                    ? {
+                        color: '#ffffff',
+                        backgroundColor: '#3f54d1',
+                        cursor: 'pointer',
+                      }
+                    : {
+                        color: '#3f54d1',
+                        backgroundColor: 'rgba(63, 84, 209, 0.1)',
+                      }
+                }
+              >
+                중복체크
+              </button>
             </div>
+            <span
+              style={validation ? { color: '#ffffff' } : { color: '#f42a3b' }}
+            >
+              숫자, 특수문자, 공백 제외 최소 2자~10자까지 입력
+            </span>
           </div>
           <div className={styles.numinputarea}>
             휴대폰번호
@@ -88,6 +144,27 @@ const EditInfo = () => {
             </div>
             <div className={styles.btnarea}>
               <button>회원 탈퇴</button>
+            </div>
+          </div>
+        ) : null}
+        {filemodal ? (
+          <div className={styles.filemodalcontain}>
+            <div className={styles.filebtns}>
+              <div className={styles.choosefilebtns}>
+                <label htmlFor="file">
+                  <div className={styles.upload}>사진 선택하기</div>
+                </label>
+                <input
+                  type="file"
+                  name="file"
+                  id="file"
+                  style={{ display: 'none' }}
+                />
+                <button>기본 이미지로 변경</button>
+              </div>
+              <div className={styles.exitbtn}>
+                <button onClick={() => setFilemodal(false)}>취소</button>
+              </div>
             </div>
           </div>
         ) : null}
