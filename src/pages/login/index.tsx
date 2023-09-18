f8766451d4c1bb3fbf4ff89df266bb7a4e2c2405
azure0929 <chary8028@naver.com>
2023-09-18 14:45:14 +0900

    #73 Feat: login 레이아웃 작업

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,41 @@
 import Background from "@/components/Background";
 import MainTab from "@/components/MainTab";
+import login_screen from "@/assets/login_screen.svg";
+import kakao from "@/assets/kakao.svg";
+import styles from "@/styles/login.module.scss";
 
 const Login = () => {
   return (
     <>
       <Background>
-        
+        <div className={styles.container}>
+          <div className={styles.head}></div>
+          <div className={styles.contents}>
+            <div className={styles.title}>
+              <p>
+                반려동물과의 건강한 <br />
+                라이프 챌린지
+              </p>
+              <span>동료 집사분들과 함께 해볼까요?</span>
+            </div>
+            <div
+              role="button"
+              className={styles.kakao}
+            >
+              <img src={kakao} alt="kakao-icon" />
+              <span>카카오로 간편 로그인</span>
+            </div>
+            <div className={styles.chat}>
+              <span>로그인이 안되시나요?</span> |
+              <span>1 : 1 채팅 상담하기</span>
+            </div>
+          </div>
+          <img
+            className={styles.back}
+            src={login_screen}
+            alt="login_screen"
+          />
+        </div>
       </Background>
       <MainTab />
     </>
