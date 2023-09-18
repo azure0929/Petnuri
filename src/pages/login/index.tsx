78d06691bf9d31e1abceb7e880a1c44308a3952f
azure0929 <chary8028@naver.com>
2023-09-18 14:32:25 +0900

    #73 Feat: login 페이지 생성

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import Background from "@/components/Background";
+import MainTab from "@/components/MainTab";
+
+const Login = () => {
+  return (
+    <>
+      <Background>
+        
+      </Background>
+      <MainTab />
+    </>
+  );
+};
+
+export default Login;
