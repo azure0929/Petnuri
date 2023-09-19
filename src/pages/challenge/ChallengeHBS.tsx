9e435025f415a19ea96fe2860ace83fca5e7e94e
hwanginseung <hwanginseung@naver.com>
2023-09-19 13:41:34 +0900

    #2 Design:챌린지 HBS 디자인 변경사항 수정

--- src/pages/challenge/ChallengeHBS.tsx
+++ src/pages/challenge/ChallengeHBS.tsx
@@ -6,9 +6,9 @@ import checkGray from '@/assets/check_circle_gray.svg'
 import checkBlue from '@/assets/check_circle_blue.svg'
 
 const ChallengeHBS = () => {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState<number | null>(null);
 
-  const handleClick = (index) => {
+  const handleClick = (index:number) => {
     if (selected === index) {
       setSelected(null);
     } else {
@@ -28,7 +28,7 @@ const ChallengeHBS = () => {
         <div className={styles.attend}>
           <div className={styles.text_box}>
             <span className={styles.title}>출석체크</span>
-            <div className={styles.sub}>출석석체크하고 30 포인트 받아가세요 !</div>
+            <div className={styles.sub}>출석석체크하고 10 포인트 받아가세요!</div>
           </div>
           <button className={styles.btn_1} onClick={complete}>포인트 받기</button>
         </div>
@@ -37,14 +37,13 @@ const ChallengeHBS = () => {
           <div className={styles.text_box}>
             <span className={styles.title}>집사퀴즈</span>
             <div className={styles.sub}>
-            집사 퀴즈 풀고 50포인트 받아가세요 !
+            집사에겐 필수 상식! 집사 퀴즈 풀고 20포인트 받아가세요 !
             </div>
           </div>
-          <button className={styles.btn_1} onClick={wrong}>지급완료</button>
         </div>
 
         <div className={styles.quetion}>
-          <div>Q. 다음 중 강아지, 고양이에게 절대 먹이면 안되는 음식은?</div>
+          <span>Q. 다음 중 강아지, 고양이에게 절대 먹이면 안되는 음식은?</span>
         </div>
         <div className={styles.answer}>
           <div className={styles.selected} onClick={() => handleClick(0)}> 
@@ -71,8 +70,7 @@ const ChallengeHBS = () => {
           <div className={styles.text_box}>
             <span className={styles.title}>친구초대</span>
             <div className={styles.sub}>
-              추천 코드를 통해 친구가 회원가입을 할 경우 
-              <br />나와 내 친구에게 각각 2000 포인트를 드립니다 !
+              회원가입시 추천코드를 입력하면 나도 친구도 모두 2000포인트 지급! 
             </div>
           </div>
         </div>
