be0e66569a9f549bd3da23a88a88a2a171e1e5b5
hwanginseung <hwanginseung@naver.com>
2023-10-04 23:03:32 +0900

    #12 Fix:login modal 떠야하는 부분 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -137,7 +137,7 @@ const Home = () => {
                </div>
                {/* 데이터가 있을 때는 "수정하기", 없을 때는 "등록하기"를 표시 */}
                <div className={styles.modify} onClick={() => 
-                petProfile.length > 0 ? onPetProfileModify() : onPetProfileAdd()}>
+                petProfile.length > 0 ? openLoginModal(onPetProfileModify) : openLoginModal(onPetProfileAdd)}>
                  {petProfile.length > 0 ? '수정하기' : '등록하기'}
                </div>
              </div>
