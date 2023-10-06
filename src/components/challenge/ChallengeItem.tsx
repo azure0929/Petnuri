791fe8a60d02bae1074443b00888745d7be30b6c
LeejungHwan <antglljung@naver.com>
2023-10-06 15:08:16 +0900

    #35 bug: BS 버그 수정 - 2

--- src/components/challenge/ChallengeItem.tsx
+++ src/components/challenge/ChallengeItem.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Pagination from "../Pagination";
 import styles from "@/styles/challenge/challengeitem.module.scss";
-import { useScrollDiv } from "@/utils/Scroll";
 import { useSetRecoilState } from "recoil";
 import { kitModalState } from "@/store/challengeState";
 import 표지 from "@/assets/표지.png";
@@ -26,31 +25,11 @@ const ChallengeItem = () => {
     // 페이지 변경 로직 추가: 이 함수에서 페이지 변경에 필요한 동작을 수행합니다.
   };
 
-  const scrollRef = useScrollDiv();
-
-  const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
-    if (e.deltaY > 0) {
-      // 아래로 스크롤: 다음 페이지로 이동
-      if (currentPage < totalPages) {
-        setCurrentPage(currentPage + 1);
-      }
-    } else if (e.deltaY < 0) {
-      // 위로 스크롤: 이전 페이지로 이동
-      if (currentPage > 1) {
-        setCurrentPage(currentPage - 1);
-      }
-    }
-  };
-
   return (
     <>
       <div className={styles.itemContainer}>
         <div className={styles.head}>검진키트 배너</div>
-        <div
-          className={styles.imgContainer}
-          onWheel={handleWheel}
-          ref={scrollRef}
-        >
+        <div className={styles.imgContainer}>
           <div className={styles.img} onClick={() => setKitOpen(true)}>
             {/* 페이지에 해당하는 이미지 표시 */}
             <img
