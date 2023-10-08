78a28a352a458e938cabae416a56976a9346cf28
hwanginseung <hwanginseung@naver.com>
2023-10-08 20:30:16 +0900

    "#12 Fix:setcookie 도메인,path 추가"

--- src/utils/Cookie.ts
+++ src/utils/Cookie.ts
@@ -3,7 +3,7 @@ import { Cookies } from 'react-cookie'
 const cookies = new Cookies()
 
 export const setCookie = (key: string, value: any) => {
-  return cookies.set(key, value, { sameSite: 'none', secure: true })
+  return cookies.set(key, value, { domain: 'localhost', path: '/', sameSite: 'none', secure: true })
 }
 
 export const getCookie = (key: string) => {
